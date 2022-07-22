1e5c1883862175e9f0cb72ce5bfa7463371d4375
md-siam <md.siam03@gmail.com>
2022-07-22 16:54:17 +0600

    bloc package upgraded

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+## Start the server
+```
+    1- Install NodeJs
+    2- npm install -g json-server
+    3- json-server data.json
+```
+
+
 ![Solid principles](https://github.com/babakoto/flutter_SOLID_principles-clean_architecture/blob/main/SOLID%20%26%20clean%20architecture.drawio.png)
--- lib/core/constants/network_path.dart
+++ lib/core/constants/network_path.dart
@@ -1,5 +1,7 @@
+
+
 abstract class NetworkPath {
-  static const String _host = "http://10.0.2.2:3000";
+  static const String _host = "http://localhost:3000";
   static String items = "$_host/items";
-  static String findItemById(int id) => NetworkPath.items + "/$id";
+  static String findItemById(int id) => "${NetworkPath.items}/$id";
 }
